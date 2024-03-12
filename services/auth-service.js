def0739c467c968e2726fe772da3464aba751459
Kelven Rubalaine <kelvenizzy@gmail.com>
2024-03-12 18:17:00 +0200

    Add auth service

---
+++ services/auth-service.js
@@ -0,0 +1,26 @@
+import { qb } from "../qb.js";
+import crypto from 'crypto';
+import { registerError } from '../error-service.js';
+
+
+const cipherKey = crypto.createHash('sha256');
+export const login = async (username, password) => {
+    try {
+        password = cipherKey.update(password);
+        const user = await qb('users').where({ username, password }).first();
+        return user;
+    } catch (error) {
+        registerError('Error logging in', error);
+    }
+};
+
+export const register = async (user) => {
+    try {
+        user.password = cipherKey.update(user.password);
+        const newUser = await qb('users').insert(user).returning('*');
+        return newUser;
+    }
+    catch (error) {
+        registerError('Error registering user', error);
+    }
+};
