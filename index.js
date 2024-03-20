15b3cd657cfc0d91b89e48b5a246290589180ed0
Kelven Rubalaine <kelvenizzy@gmail.com>
2024-03-20 08:26:11 +0200

    Update nodejs host linten

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ cron.schedule(config.scheduled_appointment.send_at_cron, async () => {
 });
 
 try {
-    await app.listen({ port: 3000 })
+    await app.listen({ port: 3000, host: '0.0.0.0' })
   } catch (err) {
     app.log.error(err)
     process.exit(1)
