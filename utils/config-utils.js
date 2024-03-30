d093ef5bff821932f26fd9f012d6654b0cbacaff
Kelven Rubalaine <kelvenizzy@gmail.com>
2024-03-30 09:39:12 +0200

    Remove unnecessary code

--- utils/config-utils.js
+++ utils/config-utils.js
@@ -11,10 +11,8 @@ export const loadAndStartConfig = async () =>{
     console.log(`[${new Date().toISOString()}] Config file updated`);
     const scheduled_expression = genExpression(config.scheduled_time);
     const delayed_expression = genExpression(config.delayed_time);
-    console.log(`[${new Date().toISOString()}] Scheduled cron: ${config.scheduled_time} | Delayed cron: ${config.delayed_time}`);
     cron.schedule(scheduled_expression, async () => {
         await sendScheduledMessages(config.scheduled_message);
-        
     }, {
         name: CRONS.SCHEDULED
     });
